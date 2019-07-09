b7465d47e364d442955f681751a93ecd3d65c833
Santiago Enciso <whereispolaris@gmail.com>
2019-07-09 13:19:32 -0500

    Updated form id's and initialized added database credentials.

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCVzWLExJ0yRXkbt5LV9eWJLn6UJNtFTrk",
+    authDomain: "train-scheduler-1e525.firebaseapp.com",
+    databaseURL: "https://train-scheduler-1e525.firebaseio.com",
+    projectId: "train-scheduler-1e525",
+    storageBucket: "",
+    messagingSenderId: "682902303998",
+    appId: "1:682902303998:web:25b12ad9c31dc2dc"
+};
+// Initialize Firebase
